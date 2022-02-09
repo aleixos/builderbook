60729e56394c1bc93f88df747cf11b5836ff38b8
aleixos <adam.aleixos@googlemail.com>
2022-02-09 22:57:30 +0000

    added themes

--- book/1-begin/pages/_app.jsx
+++ book/1-begin/pages/_app.jsx
@@ -1,6 +1,11 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import App from 'next/app';
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import { theme } from './lib/theme';
+
 import Header from './components/Header';
 
 const propTypes = {
@@ -9,16 +14,25 @@ const propTypes = {
 };
 
 class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props;
 
     // console.log(pageProps);
 
     return (
-      <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Header {...pageProps} />
         <Component {...pageProps} />
-      </>
+      </ThemeProvider>
     );
   }
 }
